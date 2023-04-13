72758dd8a73b63eb6a96620a033294dcf8b925f5
Vinay1890 <129633599+Vinay1890@users.noreply.github.com>
2023-04-13 10:01:19 +0000

    adding A BUTTON

--- main.py
+++ main.py
@@ -10,8 +10,25 @@ lbl1.place(x=50,y=50)
 lbl2 = Label(window,text="Enter 2nd Number: ")
 lbl2.place(x=50,y=100)
 
+lbl3 = Label(window,text="Result: ")
+lbl3.place(x=80,y=150)
+
 t1 = Entry()
 t1.place(x=180,y=50)
 
 t2 = Entry()
 t2.place(x=180,y=100)
+
+t3 = Entry()
+t3.place(x=180,y=150)
+
+
+
+def add():
+  num1=int(t1.get())
+  num2=int(t2.get())
+  sum = num1 + num2
+  t3.insert(END,str(sum))
+ 
+b1 = Button(window, text="Add",command=add)
+b1.place(x=50,y=200)
